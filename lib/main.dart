218bbdc1a1749cbdaa5085030def372ac350c719
bielpdev <140040644+bielpdev@users.noreply.github.com>
2024-09-11 09:41:58 -0300

    Initial Commit - v1
    Projeto em andamento

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:despesas_pessoais/components/transaction_user.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(const ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Despesas Pessoas'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            child: const Card(
+              color: Colors.blue,
+              elevation: 5,
+              child: Text('Grafico'),
+            ),
+          ),
+          const TransactionUser(),
+        ],
+      ),
+    );
+  }
+}
